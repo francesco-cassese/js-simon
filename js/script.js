fdff8f3ad1c2c9dde507997c54e1bb19b2a44a83
Francesco Cassese <francescocassese9@gmail.com>
2026-04-07 18:03:23 +0200

    Aggiunta pagina di benvenuto e logica per far funzionare il bottone

--- js/script.js
+++ js/script.js
@@ -1,33 +1,51 @@
 'use strict';
 
 /* --- COLLEGAMENTI --- */
-const containerCountdown = document.querySelector('#countdown'); // Recupero il contenitore HTML
-const numeriDaIndovinare = document.querySelector('#numbers-list'); //Recupero il contenitore della lista numeri
+const paginaDiBenvenuto = document.querySelector('#welcome-screen');
+const bottoneInizio = document.querySelector('#start-button');
+const containerCountdown = document.querySelector('#countdown');
+const numeriDaIndovinare = document.querySelector('#numbers-list');
+const istruzioni = document.querySelector('#instructions');
+const formRisposte = document.querySelector('#answers-form');
+const campiInput = document.querySelectorAll('#input-group input');
 
 
 /* --- VARIABILI --- */
 const secondiIniziali = 5;                                      // Imposto il tempo di gioco per il Simon Says
 const quantitaNumeri = 5;                                       // Inposto i numeri che mi servono 
+let sequenzaCorretta = [];                                      // Memorizzo i numeri generati per il confronto finale
 
 
 /* --- MAIN --- */
 
-const sequenza = generaSequenzaNumerica(quantitaNumeri);        //Richiamo la funzione che mi genera i numeri 
+bottoneInizio.addEventListener('click', () => {
 
-numeriDaIndovinare.innerHTML = `${sequenza.join(' - ')}`;       //Stampo i 5 numeri nell'html
+    //Fase di Sparizione/Apparizione
+    paginaDiBenvenuto.classList.add('d-none');
+    istruzioni.classList.remove('d-none');
 
-// Chiamo la logica e gestisco gli avvisi in base ai codici 1 e 0
-gestioneContoAllaRovescia(secondiIniziali, (codice, tempoRimanente) => {
+    //Richiamo gli strumenti
+    const sequenzaCorretta = generaSequenzaNumerica(quantitaNumeri);
+    numeriDaIndovinare.innerHTML = `${sequenzaCorretta.join(' - ')}`;
 
-    if (codice === 1) {
+    //Esecuzione Timer
+    gestioneContoAllaRovescia(secondiIniziali, (codice, tempoRimanente) => {
 
-        // Gestione Avviso: Il timer è attivo
-        containerCountdown.innerHTML = `Mancano: ${tempoRimanente}s`;
-    } else if (codice === 0) {
+        if (codice === 1) {
+            //Durante il countdown
+            containerCountdown.innerHTML = `Mancano: ${tempoRimanente}s`;
+        } else {
+            //A tempo scaduto
+            containerCountdown.innerHTML = "INSERISCI I NUMERI!";
 
-        // Gestione Avviso: Il timer è arrivato a zero
-        containerCountdown.innerHTML = "TEMPO SCADUTO!";
-    }
+            //Nascondo elementi di memorizzazione
+            numeriDaIndovinare.classList.add('d-none');
+            istruzioni.classList.add('d-none');
 
+            //Mostro il form di risposta
+            formRisposte.classList.remove('d-none');
+
+            console.log("Sequenza corretta:", sequenzaCorretta);
+        }
+    });
 });
-
